d25a23dadcadbf29633278672657851cc48e22bb
Madhuash <rao.madhavi57@gmail.com>
2016-03-04 20:51:12 +0530

    Added files via upload

---
+++ IntAssignment.java
@@ -0,0 +1,9 @@
+
+public class IntAssignment {
+	public static void main(String[] args)
+	{
+		int a,b=5,c;
+		
+		System.out.println(b);
+	}
+}
---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+
+public class SumOfTwoNumbers {
+	public static void main(String[] args)
+	{
+		int a = 5;
+		float b = 32;
+		int c;
+		c=(int)(a+b);
+		System.out.println(c);
+	}
+}
